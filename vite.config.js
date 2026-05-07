dff5c93a53560e11e2c50749ed331059e6488e05
AncientOfLight <lotso_eduardo@hotmail.com>
2026-05-07 16:48:31 -0600

    Add base path to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/color-palette-and-wheel/', // <--- AGREGA ESTO EXACTAMENTE
+  base: '/color-palette-and-wheel/',
 })
