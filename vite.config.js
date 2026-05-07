b3a47c02d0c8b5f815a9d687da8fe19fb3ff3042
AncientOfLight <lotso_eduardo@hotmail.com>
2026-05-07 16:35:07 -0600

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nombre-de-tu-repositorio/', // <--- Asegúrate de que esto tenga el nombre exacto de tu repo entre barras
+  base: '/color-palette-and-wheel/', // <--- AGREGA ESTO EXACTAMENTE
 })
